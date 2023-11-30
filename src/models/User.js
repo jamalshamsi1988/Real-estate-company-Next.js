a2c8937169f371fcc3304dc63cfa67ec8c34c1c5
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-30 16:25:24 +0330

    create User Model

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+import { Schema, models, model } from "bcryptjs";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+});
+
+const User = models.User || model("User", userSchema);
+export default User;
