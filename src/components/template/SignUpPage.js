0ab133b552c737af1304d395c6e03a4f900845a4
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-12-01 17:41:30 +0330

    signup form completed

--- src/components/template/SignUpPage.js
+++ src/components/template/SignUpPage.js
@@ -1,28 +1,81 @@
-"use client"
-import Link from 'next/link';
-import  { useState } from 'react'
-import styles from './SignUpPage.module.css'
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast, Toaster } from "react-hot-toast";
+import { ThreeDots } from "react-loader-spinner";
+import styles from "./SignUpPage.module.css";
 
 const SignUpPage = () => {
-    const [email , setEmail]=useState("");
-    const [password,setPassword]=useState("")
-    const [rePassword,setRePassword]=useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const signupHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== rePassword) {
+      toast.error("رمزعبور و تکرار رمزعبور برابر نیست ");
+      return;
+    }
+    setLoading(true);
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    setLoading(false);
+    if (res.status === 201) {
+      router.push("/signin");
+    } else {
+      toast.error(data.error);
+    }
+  };
   return (
     <div className={styles.form}>
       <h4>فرم ثبت نام</h4>
       <form>
-        <label>نام کاربری</label>
-        <input type='text' value={email} onChange={e =>setEmail(e.target.value) } />
-        <label>رمز عبور</label>
-        <input type='password' value={password} onChange={e =>setPassword(e.target.value) } />
-        <label>تکرار رمز عبور</label>
-        <input type='password' value={rePassword} onChange={e =>setRePassword(e.target.value) } />
-        <button type='submit'>ورود</button>
+        <label>ایمیل :</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>رمز عبور:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label>تکرار رمز عبور:</label>
+        <input
+          type="password"
+          value={rePassword}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+        {loading ? (
+          <ThreeDots
+            height={45}
+            color="#304ffe"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{ margin: "auto" }}
+            visible={true}
+          />
+        ) : (
+          <button type="submit" onClick={signupHandler}>
+            ثبت نام
+          </button>
+        )}
       </form>
-      <p>حساب کاربری دارید ؟ <Link href='/signin'>ورود</Link></p>
-      
+      <p>
+        حساب کاربری دارید ؟ <Link href="/signin">ورود</Link>
+      </p>
+      <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
