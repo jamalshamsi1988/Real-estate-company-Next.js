0283b6a5f91c34a4c7504f4df8ab2598164f64e0
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-12-01 23:44:39 +0330

    create NextAuthProvider in a component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { yekan } from '@/utils/fonts'
 import './globals.css'
 import Layout from '@/layout/Layout'
+import NextAuthProvider from '@/providers/NextAuthProvider'
 
 
 
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir='rtl'>
       <body className={yekan.className}>
-        <Layout>
-        {children}
-        </Layout>
+        <NextAuthProvider>
+            <Layout>
+            {children}
+            </Layout>
+        </NextAuthProvider>
         </body>
     </html>
   )
