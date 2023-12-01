c8628b30e3599ee471d477dd27e7ecd2ec4116c8
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-12-01 16:22:22 +0330

    create Next auth functions

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { verifyPassword } from "@/utils/auth";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("در سرور مشکلی رخ داده است");
+        }
+        if (!email || !password)
+          throw new Error("لطفا اطلاعات معتبر وارد کنید ");
+
+        const user = await User.findOne({ email });
+        if (!user) throw new Error("لطفا حساب کاربری ایجاد کنید");
+
+        const isValid = await verifyPassword(password, user.password);
+        if (!isValid) throw new Error("ایمیل یا پسوورد اشتباه است");
+        return { email };
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export {handler as GET, handler as POST}
--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,9 +1,23 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-async function connectDB() {
-  if (mongoose.connections[0].readyState) return;
-   await mongoose.connect(process.env.MONGO_URI);
-  console.log("Connected To DB");
+// async function connectDB() {
+//   if (mongoose.connections[0].readyState) return;
+//    await mongoose.connect(process.env.MONGO_URI);
+//   console.log("Connected To DB");
+// }
+
+// export default connectDB;
+
+import mongoose from "mongoose"
+
+
+async function connectDB (){
+ 
+        if(mongoose.connections[0].readyState) return;
+        mongoose.set("strictQuery" , false)
+
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("connect to DB")
+        
 }
-
 export default connectDB;
