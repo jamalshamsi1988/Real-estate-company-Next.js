739d10bb32e46f8218a3c5981697536e7611e785
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-12-02 16:08:50 +0330

    protect the page for a user authenticate or not

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,7 +1,15 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
+
 import SignInPage from '@/template/signInPage'
 
 
-const SignIn = () => {
+const SignIn = async() => {
+  const session=await getServerSession(authOptions);
+  if(session) redirect("/");
+
   return (
     <SignInPage />
   )
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,7 +1,14 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
 import SignUpPage from "@/template/SignUpPage"
 
 
-const Signup = () => {
+const Signup =async () => {
+  const session=await getServerSession(authOptions);
+  if(session) redirect("/");
+
   return (
     <SignUpPage />
   )
