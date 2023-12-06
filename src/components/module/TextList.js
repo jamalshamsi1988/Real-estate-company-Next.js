1921df3212a2df94f0a2e1a84d78a468d598f0c6
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-06 16:20:47 +0330

    handle the change handler and delete handler

--- src/components/module/TextList.js
+++ src/components/module/TextList.js
@@ -5,8 +5,20 @@ import { AiOutlineDelete } from "react-icons/ai";
 
 const TextList = ({type , title , profileData,setProfileData}) => {
 
+  const changeHandler=(e,index)=>{
+    const {value}=e.target;
+    const list=[...profileData[type]];
+    list[index]=value;
+    setProfileData({...profileData , [type]:list})
+  }
     const addHandler=()=>{
         setProfileData({...profileData , [type]:[...profileData[type] ,""]})
+    }
+
+    const deleteHandler=(index)=>{
+      const list=[...profileData[type]];
+      list.splice(index,1);
+      setProfileData({...profileData , [type]: list})
     }
   return (
     <div className={styles.container}>
@@ -14,7 +26,8 @@ const TextList = ({type , title , profileData,setProfileData}) => {
         {
             profileData[type].map((i,index)=>(
                 <div className={styles.card} key={index}>
-                    <input type="text" />
+                    <input type="text" value={i} onChange={(e)=>changeHandler(e,index)} />
+                    <button onClick={()=>deleteHandler(index)}> حذف</button>
                 </div>
             ))
         }
