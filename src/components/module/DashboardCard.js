219ff075dc9d25caafb54b8fe7d9c8f5f634bc76
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-12 00:24:53 +0330

    handle the warning from next.js for pass the data from ssr component to client component

--- src/components/module/DashboardCard.js
+++ src/components/module/DashboardCard.js
@@ -1,10 +1,33 @@
+"use client"
 
-import Card from './Card'
+import { AiOutlineDelete } from 'react-icons/ai'
+import { FiEdit } from 'react-icons/fi'
+import Card from '@/module/Card'
+
+import styles from '@/module/DashboardCard.module.css'
+
 
 const DashboardCard = ({data}) => {
+  const editHandler=()=>{
+
+  }
+  const deleteHandler=()=>{
+
+  }
   return (
-    <div>
+    <div className={styles.container}>
      <Card data={data}/>
+     <div className={styles.main}>
+      <button onClick={editHandler}>
+        ویرایش
+        <FiEdit/>
+      </button >
+      <button onClick={deleteHandler}>
+        حذف
+        <AiOutlineDelete/>
+      </button>
+
+     </div>
     </div>
   )
 }
--- src/components/template/MyProfilesPage.js
+++ src/components/template/MyProfilesPage.js
@@ -8,7 +8,7 @@ const MyProfilesPage = ({profiles}) => {
         profiles.length ? null : <p className={styles.text}> هیچ آگهی ثبت نشده است</p>
       }
       {
-        profiles.map(i => <DashboardCard key={i._id} data={i}/>)
+        profiles.map(i => <DashboardCard key={i._id} data={JSON.parse(JSON.stringify(i))}/>)
       }
     </div>
   )
