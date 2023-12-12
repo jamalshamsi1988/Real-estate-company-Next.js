d309ae95618ed16561bd1b5a55680f86d4115293
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-12 16:10:03 +0330

    create PATCH API for editing

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -79,3 +79,85 @@ export async function POST(req) {
     );
   }
 }
+
+export async function PATCH(req) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+    const {
+      _id,
+      title,
+      description,
+      location,
+      phone,
+      price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+    } = body;
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+    if (
+      !_id ||
+      !title ||
+      !description ||
+      !location ||
+      !phone ||
+      !price ||
+      !realState ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات را به درستی وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const profile = await Profile.findOne({ _id });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        { error: "دسترسی شما به این آگهی محدود شده است" },
+        { status: 403 }
+      );
+    }
+    profile.title = title;
+    profile.description = description;
+    profile.location = location;
+    profile.phone = phone;
+    profile.price = price;
+    profile.realState = realState;
+    profile.category = category;
+    profile.constructionDate = constructionDate;
+    profile.rules = rules;
+    profile.amenities = amenities;
+    profile.save();
+    return NextResponse.json(
+      { message: "آگهی با موفقیت ویرایش شد" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
