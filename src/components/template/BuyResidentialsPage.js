0fc892ed7f929fa64229bcd3bbe89d0cf6d75405
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-15 00:12:52 +0330

    show the profiles on ui

--- src/components/template/BuyResidentialsPage.js
+++ src/components/template/BuyResidentialsPage.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import Card from '@/module/Card'
+import styles from '@/template/BuyResidentialsPage.module.css'
 
-const BuyResidentialsPage = () => {
+const BuyResidentialsPage = ({data}) => {
   return (
-    <div>
+    <div className={styles.container}>
+        <div className={styles.sidebar}></div>
+        <div className={styles.main}>
+            {
+                data.length ? null : <p>هیچ آگهی ثبت نشده است</p>
+            }
+            {
+                data.map(profile  => <Card key={profile._id} data={profile}/>)
+            }
+        </div>
      
     </div>
   )
