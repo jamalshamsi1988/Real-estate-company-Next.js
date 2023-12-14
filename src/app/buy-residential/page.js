9f5448f62e860148a6c72bcfe054bc93868f5509
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-14 23:59:41 +0330

    fetch the data from api

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,9 +1,12 @@
 import BuyResidentialsPage from '@/template/BuyResidentialsPage'
-import React from 'react'
 
-const BuyResidentials = () => {
+
+const BuyResidentials =async () => {
+  const res = await fetch("http://localhost:3000/api/profile" ,{ cache:"no-store"});
+  const data= await res.json();
+
   return (
-    <BuyResidentialsPage/>
+    <BuyResidentialsPage data={data}/>
   )
 }
 
