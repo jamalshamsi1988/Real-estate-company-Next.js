e9c0c4c77b15117fd45a7f25b4ffd95d4c37082c
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-15 17:09:29 +0330

    import icons

--- src/components/template/DetailsPage.js
+++ src/components/template/DetailsPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { SiHomebridge } from "react-icons/si";
+import { AiOutlinePhone } from "react-icons/ai";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BiCalendarCheck } from "react-icons/bi";
+import { e2p, sp } from "@/utils/replaceNumber";
+
+import { icons } from "@/constants/icons";
+import { categories } from "@/constants/strings";
+import styles from "@/template/DetailsPage.module.css";
 
 const DetailsPage = () => {
   return (
-    <div>
-      detailsPage
+    <div className={styles.container}>
+      <div className={styles.main}></div>
+      <div className={styles.sidbar}></div>
     </div>
-  )
-}
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
