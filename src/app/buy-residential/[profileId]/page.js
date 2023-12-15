04844bc383f5ecef3b4eb53e7480824015b404f9
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-15 17:04:10 +0330

    crteae Details Page

--- src/app/buy-residential/[profileId]/page.js
+++ src/app/buy-residential/[profileId]/page.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import Profile from "@/models/Profile";
+import DetailsPage from "@/template/DetailsPage";
+import connectDB from "@/utils/connectDB"
 
-const ProfilDetail = () => {
+
+const ProfilDetail = async({params :{profileId}}) => {
+ await connectDB();
+ const profile = await Profile.findOne({_id : profileId});
+ if(!profile) return <h3>مشکلی پیش آمده است</h3>
   return (
-    <div>
-      
-    </div>
+   <DetailsPage data={profile} />
   )
 }
 
